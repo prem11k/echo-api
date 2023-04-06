31b58742eb0e81ebc3574876f4cb6d5468d21edd
prem11k <m.prem11k@gmail.com>
2023-04-06 00:11:38 -0700

    Passing request headers as response

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ WORKDIR /app
 RUN export GO111MODULE=auto
 RUN cd /app && git clone https://github.com/prem11k/echo-api.git
 
-RUN cd /app/echo-api/main && go build -o /echo-api
-RUN chmod u+x /app/echo-api/main/echo-api
+RUN cd /app/echo-api/main && go build -o ./echo-api
+
 EXPOSE 3000
 
-ENTRYPOINT [ "/app/echo-api/main" ]
+ENTRYPOINT [ "/app/echo-api/main/echo-api" ]
--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,7 +18,30 @@ func handleQuery(w http.ResponseWriter, r *http.Request) {
 	for name, values := range r.Header {
 		// Loop over all values for the name.
 		for _, value := range values {
-			fmt.Println(name, value)
+			w.Header().Add(name, value)
 		}
 	}
+	w.WriteHeader(http.StatusOK)
+
+	headers := make(map[string][]string)
+	for key, values := range w.Header() {
+		headers[key] = values
+	}
+
+	response := struct {
+		Headers map[string][]string `json:"headers"`
+		Message string              `json:"message"`
+	}{
+		Headers: headers,
+		Message: "Hello, World!",
+	}
+
+	jsonResponse, err := json.MarshalIndent(response, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
 }
